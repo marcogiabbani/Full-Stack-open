c0471584ed209beed572fd0b35ddf36fd7c72e90
marco <you@example.com>
2023-01-23 18:01:17 -0300

    adds gitignore and first excerise

--- part 3/mongo.js
+++ part 3/mongo.js
@@ -1,37 +1,46 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+const url = `mongodb+srv://fullstack:${password}@cluster0.dw5dejb.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+const Contact = mongoose.model("Contact", contactSchema);
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://frankfurt:${password}@cluster0.a33mmau.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-mongoose
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+} else if (process.argv.length > 4) {
+  mongoose
     .connect(url)
     .then((result) => {
-        console.log('connected')
-
-        const note = new Note({
-            content: 'HTML is Easy',
-            date: new Date(),
-            important: true,
-        })
-
-        return note.save()
+      console.log("connected");
+      const contact = new Contact({
+        name: name,
+        number: number,
+      });
+      return contact.save();
     })
     .then(() => {
-        console.log('note saved!')
-        return mongoose.connection.close()
+      console.log(`Added ${name} number ${number} to phonebook`);
+      return mongoose.connection.close();
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
+} else {
+  console.log("entre a tercera opcion");
+  mongoose.connect(url).then((result) => {
+    Contact.find({}).then((result) => {
+      console.log("phonebook:");
+      result.forEach((person) => {
+        console.log(`${person.name} ${person.number}`);
+      });
+      return mongoose.connection.close();
+    });
+  });
+}
