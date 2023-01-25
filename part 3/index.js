7d03798f132d3112bb31948f03ae522262bdc532
marco giabbani <mtgiabbani@gmail.com>
2023-01-25 12:59:33 -0300

    adds notes own module, modifyes routes

--- part 3/index.js
+++ part 3/index.js
@@ -1,45 +1,56 @@
 
+require('dotenv').config()
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-let sentences = [
-    {
-      "id": 1,
-      "content": "HTML is easy",
-      "date": "2022-1-17T17:30:31.098Z",
-      "important": true
-    },
-    {
-      "id": 2,
-      "content": "Browser can execute only JavaScript",
-      "date": "2022-1-17T18:39:34.091Z",
-      "important": false
-    },
-    {
-      "id": 3,
-      "content": "GET and POST are the most important methods of HTTP protocol",
-      "date": "2022-1-17T19:20:14.298Z",
-      "important": true
-    },
-    {
-      "content": "hola",
-      "date": "2022-12-29T12:59:37.866Z",
-      "important": true,
-      "id": 4
-    }
-]
+const Note = require('./models/note')
+
+
+
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  console.log(request.body)
+
+  if (body.content === undefined) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const note = new Note ({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json({savedNote})
+  })
+  .catch((error) => ('Error while trying to save note', error))
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+  .catch((error) => ('Error while trying to find note by id', error))
+})
+
+
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
-app.get('/api/notes', (request, response) => {
-    response.json(sentences)
+  app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
 
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const port = process.env.PORT
+app.listen(port)
+console.log(`Server running on port ${port}`)
