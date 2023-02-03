ce5383acbf78e91fb665866e751b66d69cc3bbc6
marco <you@example.com>
2023-02-03 11:06:51 -0300

    from dell laptop

--- part3/index.js
+++ part3/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const Note = require("./models/note");
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
   console.log(request.body);
 
@@ -26,44 +26,52 @@ app.post("/api/notes", (request, response) => {
     .then((savedNote) => {
       response.json({ savedNote });
     })
-    .catch((error) => ("Error while trying to save note", error));
+    .catch((error) => next(error));
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       response.json(note);
     })
-    .catch((error) => ("Error while trying to find note by id", error));
+    .catch((error) => next(error));
 });
 
-app.put("/api/notes/:id", (request, response) => {
-  const body = request.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-
-    //aca tengo que actualizar la nota
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
-    .catch((error) => ("Error while trying to find note by id", error));
+    .catch((error) => next(error));
 });
 
-app.get("/", (request, response) => {
+app.get("/", (request, response, next) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
+app.get("/api/notes", (request, response, next) => {
   Note.find({}).then((notes) => {
     response.json(notes);
   });
 });
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
 
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 const port = process.env.PORT;
 app.listen(port);
 console.log(`Server running on port ${port}`);
--- part3/models/note.js
+++ part3/models/note.js
@@ -1,30 +1,35 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to ', url)
+console.log("connecting to ", url);
 
-mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('eror connecting to MongoDB:', error.message)
-    })
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("eror connecting to MongoDB:", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+});
 
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
