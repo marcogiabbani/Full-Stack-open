4a7329a924b10e9fca93448ed0b7c2c1decd9d88
marco <you@example.com>
2023-02-14 12:15:38 -0300

    ends intro to testing

---
+++ part4/tests/listHelper.test.js
@@ -0,0 +1,65 @@
+const listHelper = require('../utils/list_helper')
+
+test('zero likes returns 0', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    }
+  ]
+
+  const result = listHelper.totalLikes(blogs)
+  expect(result).toBe(0)
+})
+
+test('one like returns 1', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 1,
+      __v: 0
+    }
+  ]
+
+  const result = listHelper.totalLikes(blogs)
+  expect(result).toBe(1)
+})
+
+test('many blogs accumulate total likes', () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 10,
+      __v: 0
+    }
+  ]
+
+  const result = listHelper.totalLikes(blogs)
+  expect(result).toBe(15)
+})
