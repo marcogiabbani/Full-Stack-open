e2e075aea65155821e69b2f17372eb9603ac3eaa
marco giabbani <mtgiabbani@gmail.com>
2023-02-27 13:30:57 -0300

    starts superstest running ok

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -2,7 +2,10 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGO_URI = process.env.MONGO_URI
+
+const MONGO_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGO_URI
 
 module.exports = {
   MONGO_URI,
