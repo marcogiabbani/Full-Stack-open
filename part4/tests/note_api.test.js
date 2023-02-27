eeebc2548542d44b4489dc8d80eb12e1c578a906
marco giabbani <mtgiabbani@gmail.com>
2023-02-27 15:02:46 -0300

    modifies logger and adds two test failing

--- part4/tests/note_api.test.js
+++ part4/tests/note_api.test.js
@@ -1,3 +1,6 @@
+//RECORDAR QUE TEST_MONGODB_URI usa 'testNoteApp'
+//y que MONGO_URI usa 'notesApp'
+
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -5,13 +8,30 @@ const app = require('../app')
 const api = supertest(app)
 
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+}, 100000)
+
+/**
+ * (todavía no subi ningun blog ni nota a la base de
+ * datos para los tests)
+ */
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
+}, 100000)
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].content).toBe('HTML is esay')
 })
 
 afterAll(async () => {
   await mongoose.connection.close()
 })
+
