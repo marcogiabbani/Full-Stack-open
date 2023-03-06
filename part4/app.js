7d2f2fdfb5663a6ac099b504cad1d0b81ab852c2
marco <you@example.com>
2023-03-06 14:36:27 -0300

    adds notes files

--- part4/app.js
+++ part4/app.js
@@ -2,10 +2,13 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controller/blogs')
 const logger = require('./utils/logger')
-
 const mongoose = require('mongoose')
+
+const notesRouter = require('./controller/notes')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controller/blogs')
+
 mongoose.set('strictQuery', false)
 
 logger.info('Connecting to', config.MONGO_URI)
@@ -20,7 +23,11 @@ mongoose.connect(config.MONGO_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
 app.use('/api/blogs', blogsRouter)
+
 
 app.get('/api/ping', (request, response) => {
 
@@ -32,5 +39,8 @@ app.get('/api/ping', (request, response) => {
       logger.error(`Error connecting to port ${config.PORT}`, error.message)
     })
 })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
