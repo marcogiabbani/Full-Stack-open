766b5b5b4b1f24c768766090fa6aa7d218a0cedc
marco <you@example.com>
2023-03-14 16:27:07 -0300

    finishes first set of exs

--- part4/controller/blogs.js
+++ part4/controller/blogs.js
@@ -12,11 +12,26 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const blog = new Blog({ body })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
   try {
     const savedNote = await blog.save()
     response.status(201).json(savedNote)
   } catch(error) {
+    next(error)
+  }
+})
+
+blogsRouter.get('/:title', async (request, response, next) => {
+  console.log('el parametro de busqueda resquest.prams.title es', request.params.title)
+  try {
+    const blog = await Blog.find({ title: request.params.title })
+    response.status(200).json(blog)
+  } catch (error) {
     next(error)
   }
 })
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -24,9 +24,45 @@ test('blogs are returned in json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(helper.blogsInDb.length)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('blogs have id property', async () => {
+  const response = await api.get('/api/blogs')
+  const oneBlog = response.body[0]
+  expect(oneBlog.id).toBeDefined()
+})
+
+test('making an HTTP POST req increases blogs in DB by one', async () => {
+  const blog = new Blog({
+    title: 'un post super nuevo',
+    author: 'marquii',
+    url: 'www.fiff.com',
+    likes:545
+  })
+
+  await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+})
+
+test('if likes property is missing, default is 0', async () => {
+  const blog = new Blog({
+    title: 'un post sin la propiedad de likes',
+    author: 'marquii',
+    url: 'www.fiff.com',
+  })
+
+  await api.post('/api/blogs').send(blog)
+  const response = await api.get(`/api/blogs/${blog.title}`)
+  console.log('response.body.likes', response.body.likes)
+  expect(blog.likes).toEqual(0)
+
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
