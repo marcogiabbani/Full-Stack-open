7206dc2a14048ff957fe1d046fcb2da2d024723e
marco <you@example.com>
2023-03-14 14:25:18 -0300

    starts exc

--- part4/controller/blogs.js
+++ part4/controller/blogs.js
@@ -1,22 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.status(200).json(blogs)
-    })
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
+    response.status(200).json(blogs)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+  const blog = new Blog({ body })
+  try {
+    const savedNote = await blog.save()
+    response.status(201).json(savedNote)
+  } catch(error) {
+    next(error)
+  }
 })
 
 module.exports = blogsRouter
